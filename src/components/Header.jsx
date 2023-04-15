5bba2b5130039cfc028e8a549654d168fb499b2d
anushka-Dahiya <anushkadahiya22@gmail.com>
2023-04-15 16:22:30 +0530

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import '../index.css'
+
+export default function Header(){
+    return (
+       <header className="header">
+          <img  className ="img-logo "src="./images/Logo.png" alt="app-logo" />
+         
+       </header>
+    )
+}
+
